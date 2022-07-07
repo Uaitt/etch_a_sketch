5444deda386a75df8c5c522df309e374c2716aa5
Lorenzo <lorenzozabot@gmail.com>
2022-07-07 10:40:07 +0200

    Create createGrid and removeGrid functions

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+let divArr = [];
 const container = document.querySelector("#grid-container");
+let divsQuantity = createGrid(16);
 
-//create the 16x16 grid
-for (let i = 0; i < 256; i++)
-{
-    const div = document.createElement("div");
-    container.appendChild(div);
-    div.classList.add("grid-element");
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+        let gridSize = prompt("Enter the size of the new grid: ");
+        
+        //remove the grid elements from the container
+        removeGrid(divsQuantity);
 
-    //add an EventListener
-    div.addEventListener("mouseover", () => div.style.backgroundColor = "black");
+        //create a new grid
+        divsQuantity = createGrid(gridSize)
+});
+
+function createGrid(size){
+    let i;
+    for ( i = 0; i < size*size; i++)
+    {
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.classList.add("grid-element");
+
+        //add an EventListener
+        div.addEventListener("mouseover", () => div.style.backgroundColor = "black");
+
+        //store every child in an array
+        divArr[i] = div;
+    }
+
+    return i;
 }
 
+function removeGrid(divsQuantity)
+{
+    let i;
+
+    for( i = 0; i < divsQuantity; i++)
+        container.removeChild(divArr[i]);
+}
